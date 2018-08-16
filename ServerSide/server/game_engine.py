e31810e45e741afe1f31ad577b56d26f85de01f8
Evgeny Sorokin <evgerher1l@gmail.com>
2018-08-16 22:07:07 +0300

    Clean files

--- ServerSide/server/game_engine.py
+++ ServerSide/server/game_engine.py
@@ -1,11 +1,8 @@
-from monster import Monster
+from Monster import Monster
 import numpy
 import math
 
-
-
 class Field:
-
     def __init__(self):
         self.max_x = 100.0
         self.max_y = 100.0
