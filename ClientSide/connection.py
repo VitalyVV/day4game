54c67f37060a60ba691a564c6a5cfcb49aadd55d
VitalyVV <v.volobuev@innopolis.ru>
2018-08-16 22:21:15 +0300

    Save commit

--- ClientSide/connection.py
+++ ClientSide/connection.py
@@ -4,7 +4,7 @@ import requests
 def send_data(parameters):
 	url = 'http://127.0.0.1:5000/make_action'
 	try:
-		r = requests.post(url=url, params=params)
+		r = requests.post(url=url, params=parameters)
 		return r.json()
 	except ConnectionError:
 		print("Connection error occured, check your network or contact @evgerher")
